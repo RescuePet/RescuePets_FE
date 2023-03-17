eca3adfa37423d397173bcfabce3644f0cf4bc7b
음지훈 <jhmc5268@naver.com>
2023-03-17 14:36:09 +0900

    Design: Layout.jsx css 색상변경

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -21,7 +21,7 @@ const Box = styled.div`
   margin: 0 auto;
   display: flex;
   justify-content: center;
-  background-color: #f9f3ea;
+  background-color: #FFFFFF;
   /* border: 1px solid red; */
 
   // 430px이하일떄 넣어줌
@@ -30,7 +30,7 @@ const Box = styled.div`
     margin: 0 auto;
     width: 375px;
     border: 1px solid blue;
-    background-color: #f9f3ea;
+    background-color: #FFFFFF;
   }
 `;
 
