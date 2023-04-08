276e41dff5378f7cc3f78af2b4a4242481698395
hdlee0619 <dlgusehd1211@naver.com>
2023-04-08 23:54:17 +0900

    Feat: 회원정보 수정 바로 반영

--- src/redux/modules/infoeditSlice.js
+++ src/redux/modules/infoeditSlice.js
@@ -1,48 +1,49 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { instance } from "../../utils/api";
+import Cookies from "js-cookie";
 
 // Get adoption list
 export const __PutMyinfoEdit = createAsyncThunk(
-    "putMyinfoEdit",
-    async (payload, thunkAPI) => {
-        try {
-            const response = await instance.put('/api/member/edit', payload);
-            console.log(response);
-            return thunkAPI.fulfillWithValue(response.data);
-        } catch (error) {
-            console.log(error.response.data);
-            throw new Error(error.response.data.message);
-        }
+  "putMyinfoEdit",
+  async (payload, thunkAPI) => {
+    try {
+      const response = await instance.put("/api/member/edit", payload);
+      console.log(response);
+      const NEWUSERINFO = JSON.stringify(response.data.data);
+      Cookies.set("UserInfo", NEWUSERINFO);
+      return thunkAPI.fulfillWithValue(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+      throw new Error(error.response.data.message);
     }
+  }
 );
 
 const initialState = {
-    error: false,
-    loading: false,
-    message: '',
+  error: false,
+  loading: false,
+  message: "",
 };
 
-
 export const putMyinfoEdit = createSlice({
-    name: "putMyinfoEdit",
-    initialState,
-    reducers: {},
+  name: "putMyinfoEdit",
+  initialState,
+  reducers: {},
 
-    extraReducers: (builder) => {
-        builder
-            .addCase(__PutMyinfoEdit.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(__PutMyinfoEdit.fulfilled, (state, action) => {
-                state.loading = false;
-                state.message = action.payload;
-            })
-            .addCase(__PutMyinfoEdit.rejected, (state) => {
-                state.loading = false;
-                state.error = true;
-            });
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(__PutMyinfoEdit.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(__PutMyinfoEdit.fulfilled, (state, action) => {
+        state.loading = false;
+        state.message = action.payload;
+      })
+      .addCase(__PutMyinfoEdit.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      });
+  },
 });
-
 
 export default putMyinfoEdit.reducer;
